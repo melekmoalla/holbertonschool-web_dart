38207733fd2ed198a709bbee641e11568f7ea19d
melekmoalla <melekmoalla2468@gmail.com>
2025-03-30 18:18:30 +0100

    test and correct task 4

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -2,7 +2,7 @@ void outer(String name, String id) {
   String inner() {
   List<String> nameParts = name.split(' ');
 
-    return "Hello Agent B.${nameParts[1]} your id is $id";
+    return "Hello Agent B.${nameParts[0]} your id is $id";
   }
   
   print(inner());
