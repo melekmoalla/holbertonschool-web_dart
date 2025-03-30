09b054cfa7b456b79d6921aa58e47db847b8c66e
melekmoalla <melekmoalla2468@gmail.com>
2025-03-30 03:56:08 +0100

    task 6

---
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -0,0 +1,14 @@
+
+void main(List<String> args) {
+   int nb = int.parse(args[0]);
+    String result = '';
+    if (nb ==0){
+        result = 'zero';
+    } if( nb > 0){
+        result = 'positive';
+    }else{
+        result = 'negative';
+    }
+    print('$nb is $result');
+}
+
