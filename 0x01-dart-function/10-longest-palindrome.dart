0f8b5314e7de7559dc807dbf582ead3cb2e32dd3
melekmoalla <melekmoalla2468@gmail.com>
2025-03-30 18:10:54 +0100

    correct task 10

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,23 +1,23 @@
-bool isPalindrome(String s){
-
-    if (s.length < 3) {
-        return false;
-    }
-    String reversed = s.split('').reversed.join('');
-    return s == reversed;
+bool isPalindrome(String s) {
+  if (s.length < 3) {
+    return false;
+  }
+  String reversed = s.split('').reversed.join('');
+  return s == reversed;
 }
 
-String longestPalindrome(String s){
-    String longest = "";
-    int length = s.length;
-    
-    for (int i = 0; i < length; i++) {
-        for (int j = i + 1; j <= length; j++) {
-        String substring = s.substring(i, j);
-        if (isPalindrome(substring) && substring.length > longest.length) {
-            longest = substring;
-        }
-        }
+String longestPalindrome(String s) {
+  String longest = '';
+  int length = s.length;
+
+  for (int i = 0; i < length; i++) {
+    for (int j = i + 1; j <= length; j++) {
+      String substring = s.substring(i, j);
+      if (isPalindrome(substring) && substring.length > longest.length) {
+        longest = substring;
+      }
     }
-    return longest;
+  }
+
+    return longest.isEmpty ? "none" : longest;
 }
