3675e5b2d95bad4b8e71eb6a89828bc61a525d6d
melekmoalla <melekmoalla2468@gmail.com>
2025-03-30 17:37:04 +0100

    test

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,19 +1,25 @@
 String longestUniqueSubstring(String str) {
-    if (str == null || str.length == 0) {
+  if (str == null || str.length == 0) {
     return "";
+  }
+  List<String> list = [];
+  String return_str = '';
+  int maxLength = 0;
+
+  for (int i = 0; i < str.length; i++) {
+
+    if (!list.contains(str[i])) {
+      list.add(str[i]);
+    } else {
+      list.remove(str[i]);
+      list.add(str[i]);
     }
-    List<String> list = [];
-
-    for (int i = 0; i < str.length; i++) {
-        if (i == str.indexOf(str[i])){
-            list.add(str[i]);
-        }
-        else{
-
-            list.remove(str[i]);
-            list.add(str[i]);
-        }
+    
+    if (list.length > maxLength) {
+      maxLength = list.length;
+      return_str = list.join('');
     }
+  }
 
-    return list.join('');
+  return return_str;
 }
