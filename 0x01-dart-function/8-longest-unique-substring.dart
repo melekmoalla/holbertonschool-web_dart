33594b9aae60d894edbeaea8adcda115b1ee6a09
melekmoalla <melekmoalla2468@gmail.com>
2025-03-30 17:06:24 +0100

    task 8 test

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,10 +1,13 @@
 String longestUniqueSubstring(String str) {
-  String  return_str = '';
-  int maxLength = 0;
-  for (int i = 0; i < str.length; i++) {
-    if (i == str.indexOf(str[i])){
-        return_str += str[i];
+    if (str == null || str.length == 0) {
+    return "";
     }
-  }
-  return return_str;
+    String  return_str = '';
+    int maxLength = 0;
+    for (int i = 0; i < str.length; i++) {
+        if (i == str.indexOf(str[i])){
+            return_str += str[i];
+        }
+    }
+    return return_str;
 }
--- 0x01-dart-function/8-main.dart
+++ 0x01-dart-function/8-main.dart
@@ -3,5 +3,5 @@ import '8-longest-unique-substring.dart';
 main() {
   print(longestUniqueSubstring("abcacbd"));
   print(longestUniqueSubstring("aaaaaaaa"));
-  print(longestUniqueSubstring("abcde"));
+  print(longestUniqueSubstring(""));
 }
