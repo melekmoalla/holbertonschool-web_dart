c9e2491b6c1d53a24354f7cfc56026d2e7619648
melekmoalla <melekmoalla2468@gmail.com>
2025-03-31 03:58:55 +0100

    task 3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,16 @@
+class Password{
+    String _password = "";
+    Password({required String password}) : _password = password;
+    
+    bool isValid() {
+        return _password.length >= 8 &&
+               RegExp(r'[A-Z]').hasMatch(_password) && 
+               RegExp(r'[a-z]').hasMatch(_password) &&
+               RegExp(r'\d').hasMatch(_password);
+    }
+
+    @override
+    String toString() {
+        return "Your Password is: $_password";
+    }
+}
