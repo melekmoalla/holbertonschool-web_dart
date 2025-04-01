f7981a31f52afddf0efb4bcffce2bd14955b7127
melekmoalla <melekmoalla2468@gmail.com>
2025-04-02 00:08:15 +0100

    task 4  0x03-asynchronous

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,4 +1,22 @@
+import 'dart:convert';
+import '4-util.dart';
 
- Future<void> usersCount(){
-    
- }
+Future<double> calculateTotal() async {
+try{
+    String data = await fetchUserData();
+    String id = json.decode(data)["id"];
+    String orders = await fetchUserOrders(id);
+    List ordersList = json.decode(orders);
+
+    double total = 0.0;
+    for (String order in ordersList) {
+        String price = await fetchProductPrice(order);
+        double prices = (json.decode(price) as num).toDouble(); 
+        total += prices;
+    }
+    return total;
+  } catch (e) {
+    print("Error: $e");
+    return -1;
+  }
+}
